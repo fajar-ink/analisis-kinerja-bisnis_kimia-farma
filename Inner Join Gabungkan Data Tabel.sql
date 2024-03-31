d72486d51737092e9bff4a27132b00274d6efce0
Fajar Sugeng Ariadi <57746316+fajar-ink@users.noreply.github.com>
2024-03-31 10:53:51 +0700

    Update Inner Join Gabungkan Data Tabel.sql

--- Inner Join Gabungkan Data Tabel.sql
+++ Inner Join Gabungkan Data Tabel.sql
@@ -1,11 +1,12 @@
 SELECT ft.transaction_id, ft.date, kc.branch_id, kc.branch_name, kc.kota, kc.provinsi, kc.rating as rating_cabang, ft.customer_name, p.product_id, p.product_name, p.price as actual_price, ft.discount_percentage,
 CASE
-  when p.price <= 50000 then 10
-  when p.price between 50000 and 100000 then 15
-  when p.price between 100000 and 300000 then 20
-  when p.price between 300000 and 500000 then 25
-  else 30
-end as persentassee_gross_laba
+  when p.price <= 50000 then 0.1
+  when p.price between 50000 and 100000 then 0.15
+  when p.price between 100000 and 300000 then 0.2
+  when p.price between 300000 and 500000 then 0.25
+  else 0.3
+end as persentase_gross_laba
+
 
 FROM rakamin-kf-analytics-418715.kimia_farma.kf_final_transaction as ft
 INNER JOIN rakamin-kf-analytics-418715.kimia_farma.kf_kantor_cabang as kc
