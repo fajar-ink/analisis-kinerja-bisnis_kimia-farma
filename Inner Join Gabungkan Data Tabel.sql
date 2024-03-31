afd5acddfb2d1580001ca0a1721a7d44ae64068a
Fajar Sugeng Ariadi <57746316+fajar-ink@users.noreply.github.com>
2024-03-31 15:11:43 +0700

    Update Inner Join Gabungkan Data Tabel.sql

--- Inner Join Gabungkan Data Tabel.sql
+++ Inner Join Gabungkan Data Tabel.sql
@@ -7,7 +7,13 @@ CASE
   else 0.3
 end as persentase_gross_laba,
 p.price-(p.price * ft.discount_percentage) as nett_sales,
-nett_sales * persentase_gross_laba as nett_profit
+CASE
+  when p.price <= 50000 then 0.1 * (p.price-(p.price * ft.discount_percentage))
+  when p.price between 50000 and 100000 then 0.15 * (p.price-(p.price * ft.discount_percentage))
+  when p.price between 100000 and 300000 then 0.2 * (p.price-(p.price * ft.discount_percentage))
+  when p.price between 300000 and 500000 then 0.25 * (p.price-(p.price * ft.discount_percentage))
+  else 0.3 * (p.price-(p.price * ft.discount_percentage))
+end as nett_profit
 
 
 FROM rakamin-kf-analytics-418715.kimia_farma.kf_final_transaction as ft
@@ -15,6 +21,4 @@ INNER JOIN rakamin-kf-analytics-418715.kimia_farma.kf_kantor_cabang as kc
 ON ft.branch_id = kc.branch_id
 INNER JOIN rakamin-kf-analytics-418715.kimia_farma.kf_product as p
 ON ft.product_id = p.product_id
-#INNER JOIN rakamin-kf-analytics-418715.kimia_farma.kf_analisis_kinerja_bisnis as ks
-#ON ft.branch_id = ks.branch_id
 ;
