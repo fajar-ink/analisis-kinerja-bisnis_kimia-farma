33e8c13d846d254deb289129b41cc21fea503226
Fajar Sugeng Ariadi <57746316+fajar-ink@users.noreply.github.com>
2024-03-31 10:28:14 +0700

    Update Inner Join Gabungkan Data Tabel.sql

--- Inner Join Gabungkan Data Tabel.sql
+++ Inner Join Gabungkan Data Tabel.sql
@@ -1,4 +1,4 @@
-SELECT ft.transaction_id, ft.date, kc.branch_id, kc.branch_name, kc.kota, kc.provinsi, kc.rating as rating_cabang, ft.customer_name, p.product_id, p.product_name
+SELECT ft.transaction_id, ft.date, kc.branch_id, kc.branch_name, kc.kota, kc.provinsi, kc.rating as rating_cabang, ft.customer_name, p.product_id, p.product_name, p.price as actual_price, ft.discount_percentage, 
 
 FROM rakamin-kf-analytics-418715.kimia_farma.kf_final_transaction as ft
 INNER JOIN rakamin-kf-analytics-418715.kimia_farma.kf_kantor_cabang as kc
