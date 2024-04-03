5856e2ffd2ea6c2c4e3a368dba395a7370280cb9
Fajar Sugeng Ariadi <57746316+fajar-ink@users.noreply.github.com>
2024-04-03 09:39:54 +0700

    Update Inner Join Gabungkan Data Tabel.sql

--- Inner Join Gabungkan Data Tabel.sql
+++ Inner Join Gabungkan Data Tabel.sql
@@ -1,7 +1,15 @@
 CREATE TABLE kimia_farma.contoh_table AS
-SELECT transaction_id, date, branch_id, kota, provinsi, rating_cabang, customer_name, product_id, product_name, actual_price
+SELECT transaction_id, date, branch_id, kota, provinsi, rating_cabang, customer_name, product_id, product_name, actual_price, discount_percentage, persentase_gross_laba,
 FROM (
-  SELECT ft.transaction_id, ft.date, kc.branch_id, kc.kota, kc.provinsi, kc.rating rating_cabang, ft.customer_name, p.product_id, p.product_name, p.price actual_price
+  SELECT ft.transaction_id, ft.date, kc.branch_id, kc.kota, kc.provinsi, kc.rating rating_cabang, ft.customer_name, p.product_id, p.product_name, p.price actual_price, ft.discount_percentage,
+  CASE
+    WHEN p.price <= 50000 THEN 0.10
+    WHEN p.price > 50000 AND p.price <= 100000 THEN 0.15
+    WHEN p.price > 100000 AND p.price <= 300000 THEN 0.20
+    WHEN p.price > 300000 AND p.price <= 500000 THEN 0.25
+    WHEN p.price > 500000 then 0.30
+  END persentase_gross_laba,
+
 
   FROM kimia_farma.kf_final_transaction AS ft
   INNER JOIN kimia_farma.kf_kantor_cabang AS kc
