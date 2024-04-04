599d8b35d9b36a03d765e39f190c09cfb09c8c5d
Fajar Sugeng Ariadi <57746316+fajar-ink@users.noreply.github.com>
2024-04-04 13:45:13 +0700

    Top 5 Rating Cabang Tertinggi dan Rating Transaksi Terendah.sql

--- Rating Cabang Tertinggi dan Rating Transaksi Terendah.sql
+++ Rating Cabang Tertinggi dan Rating Transaksi Terendah.sql
@@ -4,4 +4,6 @@ SELECT provinsi, rating_cabang, rating_transaksi
 FROM (
   SELECT provinsi, rating_cabang, rating_transaksi FROM `kimia_farma.kf_analisis_kinerja_bisnis`
   ORDER BY rating_cabang DESC, rating_transaksi ASC
-);
+)
+  LIMIT 5
+;
